a82694db195a90cd1bfc4a7c57aef7736e7a0126
HeimolaUT <heimola@ut.ee>
2026-04-15 22:40:37 +0300

    updating districts

--- map1/js/javascript.js
+++ map1/js/javascript.js
@@ -12,6 +12,32 @@ function popUPinfo(feature, layer) {
   layer.bindPopup(feature.properties.NIMI)
 }
 
+// get colors from feature property
+
+function getColor(property) {
+    if (property > 500) {
+        return '#1486ff';
+    } else if (property > 200) {
+        return '#469cf8';
+    } else if (property > 100) {
+        return '#76b8ff';
+    } else if (property > 50) {
+        return '#a1ceff';
+    } else {
+        return '#c2e0ff';
+    }
+}
+// polygon style
+function polygonStyle(feature) {
+    return {
+        fillColor: getColor(feature.properties.TOWERS),
+        fillOpacity: 0.5,
+        weight: 1,
+        opacity: 1,
+        color: 'grey', 
+    }
+}
+
 // load GeoJSON
 async function addDistrictsGeoJson(url) {
     try {
@@ -20,6 +46,7 @@ async function addDistrictsGeoJson(url) {
 
         const polygons = L.geoJson(data, {
             onEachFeature: popUPinfo,
+            style: polygonStyle,
         });
 
         polygons.addTo(map); 
