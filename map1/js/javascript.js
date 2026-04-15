78b696b6b36623081d8bc52c89c842761cd9c9ce
HeimolaUT <heimola@ut.ee>
2026-04-15 22:06:45 +0300

    updating map

---
+++ map1/js/javascript.js
@@ -0,0 +1,32 @@
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+const osm =L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'OpenSteetMap contributors',
+})
+
+osm.addTo(map)
+
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+  layer.bindPopup(feature.properties.NIMI)
+}
+
+// load GeoJSON
+async function addDistrictsGeoJson(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const polygons = L.geoJson(data, {
+            onEachFeature: popUPinfo,
+        });
+
+        polygons.addTo(map); 
+    } catch (error) {
+        console.error("Geojson loading error:", error);
+    }
+}
+
+addDistrictsGeoJson('js/data/tartu_city_districts_edu.geojson');
+
