e44057c51ec133f781b6d3ab27eb16d225a92b16
HeimolaUT <heimola@ut.ee>
2026-04-16 12:35:07 +0300

    update to layers

--- map1/js/javascript.js
+++ map1/js/javascript.js
@@ -7,10 +7,17 @@ const osm =L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 osm.addTo(map)
 
 
+
+
+
 // add popup to each feature
 function popUPinfo(feature, layer) {
-  layer.bindPopup(feature.properties.NIMI)
+  layer.bindPopup(
+    feature.properties.NIMI + "<br>" +  feature.properties.TOWERS
+);
 }
+
+
 
 // get colors from feature property
 
@@ -56,4 +63,41 @@ async function addDistrictsGeoJson(url) {
 }
 
 addDistrictsGeoJson('js/data/tartu_city_districts_edu.geojson');
+
+
+// point circle function
+function createCircle(feature, latlng) {
+    let options = {
+        radius: 5,
+        fillColor: 'red',
+        fillOpacity: 0.5,
+        color: 'red',
+        weight: 1,
+        opacity: 1,
+    }
+    return L.circleMarker(latlng, options)
+}
+
+
+// add geoJSON point layer
+async function addCelltowersGeoJson(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        const markers = L.geoJson(data, {
+            pointToLayer: createCircle,
+            onEachFeature: popUPinfo
+        });
+        
+        const clusters = L.markerClusterGroup()
+        clusters.addLayer(markers);
+        clusters.addTo(map);
+    } catch (error) {
+        console.error("Celltowers loading error", error);
+    }
+}
+
+
+addCelltowersGeoJson('js/data/tartu_city_celltowers_edu.geojson');
 
